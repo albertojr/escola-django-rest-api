e00a14715259bb877e33a99e7a2cdbea060eb9a2
albertojr <alberto.crato13@gmail.com>
2021-05-24 16:33:32 -0300

    CRUD com viewset e paginaçao

--- escola/settings.py
+++ escola/settings.py
@@ -2,6 +2,14 @@
 
 from pathlib import Path
 import os
+from decouple import config
+
+
+"""
+    Token:
+    c4495f64785e06ca7a0ee960d01281047c9d9b85
+
+"""
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -12,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nypg5n#ay5c7qzvv@o(@@xhb&i4%2gn!sy=^krd-=b0o2s#@6)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -32,6 +40,7 @@ INSTALLED_APPS = [
 
     'django_filters',
     'rest_framework',
+    'rest_framework.authtoken',
 
     'cursos',
 ]
@@ -127,7 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Configurações DRF
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
