be8046b556591c886c8f9df420bd1db3a100102c
albertojr <alberto.crato13@gmail.com>
2021-05-25 09:11:35 -0300

    implementando throttle - limitando requisiçoes

--- escola/settings.py
+++ escola/settings.py
@@ -145,5 +145,14 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-   'PAGE_SIZE': 2
+   'PAGE_SIZE': 2,
+   #configuração de limite de requisição
+   'DEFAULT_THROTTLE_CLASSES':(
+       'rest_framework.throttling.AnonRateThrottle',
+       'rest_framework.throttling.UserRateThrottle',
+   ),
+   'DEFAULT_THROTTLE_RATES':{
+       'anon' : '5/minute', #5 requisições por minuto para usuario anonimos
+       'user' : '10/minute' #10 requisições por minuto para usuarios registrados
+   }
 }
